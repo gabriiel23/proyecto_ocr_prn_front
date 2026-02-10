16acb2415674bac5d194513f555edf0db4ef118e
merchanjair1 <merchanjair1@gmail.com>
2026-02-10 07:18:44 -0500

    Cambio url

--- lib/core/config/backend_config.dart
+++ lib/core/config/backend_config.dart
@@ -6,7 +6,7 @@ class BackendConfig {
   // Para desarrollo local: 'http://localhost:3000'
   // Para red local: 'http://192.168.X.X:3000' (reemplazar con tu IP)
   // IP Wi-Fi de la PC: 192.168.1.92
-  static const String baseUrl = 'https://99c2-190-12-13-17.ngrok-free.app';
+  static const String baseUrl = 'https://project-cloud-ipvg.onrender.com';
 
   // Endpoints
   static const String consultarServicioOCR = '/cloud/consultar-servicio-ocr';
@@ -15,7 +15,7 @@ class BackendConfig {
   static String get consultarServicioUrl => '$baseUrl$consultarServicioOCR';
 
   // Tipos de servicio disponibles
-  static const List<String> serviceTypes = ['luz_loja', 'sri_matriculacion'];
+  static const List<String> serviceTypes = ['luz_loja', 'sri_matriculacion','ocr_cedula','ant_multas','claro_planes'];
 
   /// Mapea el enum ServiceType del frontend al string esperado por el backend
   static String getServiceTypeString(ServiceType service) {
